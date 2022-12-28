180693caf90030acad85bedbeff821ab4a1f2596
BujjuIsDumb <91763194+BujjuIsDumb@users.noreply.github.com>
2022-12-27 18:23:54 -0600

    Add Rich Presence add-on

---
+++ DiscordIntegration.Rpc/Entities/RichPresence.cs
@@ -0,0 +1,90 @@
+﻿// MIT License
+//
+// Copyright(c) 2022 Bujju
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using DiscordIntegration.Rpc.SdkWrapper;
+
+namespace DiscordIntegration.Rpc.Entities
+{
+    public sealed class RichPresence
+    {
+        public string State { get; set; }
+
+        public string Details { get; set; }
+
+        public RichPresenceTimestamp Timestamp { get; set; }
+
+        public RichPresenceMedia LargeImage { get; set; }
+
+        public RichPresenceMedia SmallImage { get; set; }
+
+        public RichPresence WithState(string state)
+        {
+            State = state;
+            return this;
+        }
+
+        public RichPresence WithDetails(string details)
+        {
+            Details = details;
+            return this;
+        }
+
+        public RichPresence WithTimestamp(RichPresenceTimestamp timestamp)
+        {
+            Timestamp = timestamp;
+            return this;
+        }
+
+        public RichPresence WithLargeImage(RichPresenceMedia largeImage)
+        {
+            LargeImage = largeImage;
+            return this;
+        }
+
+        public RichPresence WithSmallImage(RichPresenceMedia smallImage)
+        {
+            SmallImage = smallImage;
+            return this;
+        }
+
+        internal Activity ToActivity()
+        {
+            return new Activity
+            {
+                State = State,
+                Details = Details,
+                Timestamps =
+                {
+                    Start = ((DateTimeOffset)Timestamp?.Start).ToUnixTimeSeconds(),
+                    End = ((DateTimeOffset)Timestamp?.End).ToUnixTimeSeconds()
+                },
+                Assets =
+                {
+                    LargeImage = LargeImage?.ImageKey,
+                    LargeText = LargeImage?.Tooltip,
+                    SmallImage = SmallImage?.ImageKey,
+                    SmallText = SmallImage?.Tooltip
+                }
+            };
+        }
+    }
+}
---
+++ DiscordIntegration.Rpc/SdkWrapper/ImageManager.cs
@@ -0,0 +1,33 @@
+// This is a slightly modified version of the Discord Game SDK.
+// More info: https://discord.com/developers/docs/game-sdk/sdk-starter-guide
+
+namespace DiscordIntegration.Rpc.SdkWrapper
+{
+    internal partial struct ImageHandle
+    {
+        public static ImageHandle User(long id) => User(id, 128);
+
+        public static ImageHandle User(long id, uint size)
+        {
+            return new ImageHandle
+            {
+                Type = ImageType.User,
+                Id = id,
+                Size = size,
+            };
+        }
+    }
+
+    internal partial class ImageManager
+    {
+        public void Fetch(ImageHandle handle, FetchHandler callback) => Fetch(handle, false, callback);
+
+        public byte[] GetData(ImageHandle handle)
+        {
+            var dimensions = GetDimensions(handle);
+            byte[] data = new byte[dimensions.Width * dimensions.Height * 4];
+            GetData(handle, data);
+            return data;
+        }
+    }
+}
